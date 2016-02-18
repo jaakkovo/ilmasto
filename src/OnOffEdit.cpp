ebd7b6c8a8f0f876ac38e3ad81318e02835cc952
jaakkovo <jaakko.voutilainen@metropolia.fi>
2016-02-18 12:05:00 +0200

    Jaakon muutokset
    Tässä tehty OnOffEdit -luokka.

---
+++ src/OnOffEdit.cpp
@@ -0,0 +1,86 @@
+/*
+ * OnOffEdit.cpp
+ *
+ *  Created on: 18.2.2016
+ *      Author: Jaakko
+ */
+
+#include "OnOffEdit.h"
+#include <cstdio>
+
+OnOffEdit::OnOffEdit(LiquidCrystal& lcd_, std::string editTitle): lcd(lcd_), title(editTitle){
+	value = 0;
+	edit = 0;
+	focus = false;
+}
+
+OnOffEdit::~OnOffEdit() {
+	// TODO Auto-generated destructor stub
+}
+
+void OnOffEdit::increment() {
+	if (value == true){
+		edit = false;
+	} else{
+		edit = true;
+	}
+}
+
+void OnOffEdit::decrement() {
+	if (value == true){
+		edit = false;
+	} else{
+		edit = true;
+	}
+}
+
+void OnOffEdit::accept() {
+	save();
+}
+
+void OnOffEdit::cancel() {
+	edit = value;
+}
+
+void OnOffEdit::setFocus(bool focus) {
+	this->focus = focus;
+}
+
+void OnOffEdit::display() {
+	lcd.clear();
+	lcd.setCursor(0,0);
+	lcd.print(title);
+	lcd.setCursor(0,1);
+	char s[16];
+	if(focus) {
+		//bar.draw(12);
+		//snprintf(s, 16, "     [%4d]     ", edit);
+		v1 = edit-lower;
+
+		v2 = upper-lower;
+
+		v3 = v1/v2;
+
+		bar.draw(v3*80);
+	}
+	else {
+		snprintf(s, 16, "      %4d      ", edit);
+		lcd.print(s);
+	}
+}
+
+void OnOffEdit::save() {
+	// set current value to be same as edit value
+	value = edit;
+	// todo: save current value for example to EEPROM for permanent storage
+}
+
+int OnOffEdit::getValue() {
+	return value;
+}
+
+void OnOffEdit::setValue(bool value) {
+	edit = value;
+	save();
+}
+
---
+++ src/OnOffEdit.h
@@ -0,0 +1,39 @@
+/*
+ * OnOffEdit.h
+ *
+ *  Created on: 18.2.2016
+ *      Author: Jaakko
+ */
+
+#ifndef ONOFFEDIT_H_
+#define ONOFFEDIT_H_
+
+#include "PropertyEdit.h"
+#include "BarGraph.h"
+#include "LiquidCrystal.h"
+#include <string>
+
+class OnOffEdit : public PropertyEdit {
+public:
+	OnOffEdit(LiquidCrystal& lcd_, std::string editTitle);
+	virtual ~OnOffEdit();
+	void increment();
+	void decrement();
+	void accept();
+	void cancel();
+	void setFocus(bool focus);
+	void display();
+	int getValue();
+	void setValue(bool value);
+
+private:
+	void save();
+	void displayEditValue();
+	LiquidCrystal& lcd;
+	std::string title;
+	bool value;
+	bool edit;
+	bool focus;
+};
+
+#endif /* ONOFFEDIT_H_ */
