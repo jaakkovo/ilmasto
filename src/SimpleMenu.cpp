db1f783a724f796afd9654b8a96216350b8d21e7
tanelili <taneli.liljasto@metropolia.fi>
2016-03-21 11:36:13 +0200

    asdkoasfkj

--- src/SimpleMenu.cpp
+++ src/SimpleMenu.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * SimpleMenu.cpp
  *
@@ -69,3 +70,76 @@ void SimpleMenu::event(SubMenuItem::menuEvent e) {
 		items2[position]->event(SubMenuItem::show);
 	}
 }
+=======
+/*
+ * SimpleMenu.cpp
+ *
+ *  Created on: 3.2.2016
+ *      Author: krl
+ */
+
+#include "SimpleMenu.h"
+#include <iostream>
+using namespace std;
+
+SimpleMenu::SimpleMenu() {
+	// Pelkkä menun luominen (kun yhtään valikkoa ei ole tehty), kohta on 0 ja menujen määrä 0.
+	position = 0;
+	amountofitems = 0;
+}
+
+SimpleMenu::~SimpleMenu() {
+}
+
+void SimpleMenu::addItem(MenuItem *item) {
+	// Tässä items -taulukkoon lisätään menun kohta. Samalla menujen määrä kasvaa yhdellä.
+	items1.push_back(item);
+	amountofitems++;
+}
+
+void SimpleMenu::addItem(SubMenuItem *item) {
+	// Tässä items -taulukkoon lisätään menun kohta. Samalla menujen määrä kasvaa yhdellä.
+	items2.push_back(item);
+	amountofitems++;
+}
+
+
+void SimpleMenu::event(MenuItem::menuEvent e) {
+	// Jos yhtään valikon kohtaa ei ole:
+	if (items1.size() <= 0) return;
+
+	// Jos nykyisen valikon tapahtuma ei palauta mitään
+	// Eli jos valikkoa ei ole valittu, eikä focusta ole,
+	// Liikutaan päävalikossa. Lopussa näytetään nykyinen valikko.
+	if (!items1[position]->event(e)) {
+		if (e == MenuItem::down && position < (amountofitems - 1)) {
+			position++;
+		}
+		else if (e == MenuItem::up && position > 0) {
+			position--;
+		}
+
+		items1[position]->event(MenuItem::show);
+	}
+}
+
+
+void SimpleMenu::event(SubMenuItem::menuEvent e) {
+	// Jos yhtään valikon kohtaa ei ole:
+	if(items2.size() <= 0) return;
+
+	// Jos nykyisen valikon tapahtuma ei palauta mitään
+	// Eli jos valikkoa ei ole valittu, eikä focusta ole,
+	// Liikutaan päävalikossa. Lopussa näytetään nykyinen valikko.
+	if(!items2[position]->event(e)) {
+		if (e == SubMenuItem::down && position < (amountofitems-1)) {
+			position++;
+		}
+		else if (e == SubMenuItem::up && position > 0) {
+			position--;
+		}
+
+		items2[position]->event(SubMenuItem::show);
+	}
+}
+>>>>>>> parent of 3a4741b... v0.91
--- src/ilmasto.cpp
+++ src/ilmasto.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 /*
 ===============================================================================
@@ -315,3 +316,322 @@ int main(void) {
 	}
 	return (0);
 }
+=======
+
+/*
+===============================================================================
+ Name        : main.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+ */
+#if defined (__USE_LPCOPEN)
+#if defined(NO_BOARD_LIB)
+#include "chip.h"
+#else
+#include "board.h"
+#endif
+#endif
+
+#include "ModbusMaster.h"
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include "lcd_port.h"
+#include "BarGraph.h"
+#include "LiquidCrystal.h"
+#include "SimpleMenu.h"
+#include "SubMenuItem.h"
+#include "IntegerEdit.h"
+#include "SliderEdit.h"
+#include "StatusEdit.h"
+#include "SetupEdit.h"
+#include "ManuAutoEdit.h"
+#include "OnOffEdit.h"
+#include "TimeEdit.h"
+#include "DecimalEdit.h"
+#include "PropertyEdit.h"
+#include <cr_section_macros.h>
+//#include "I2C.h"
+#include "SetupEdit.h"
+
+static volatile int counter;
+static volatile uint32_t systicks;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void SysTick_Handler(void)
+{
+	systicks++;
+	if(counter > 0) counter--;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+/* this function is required by the modbus library */
+uint32_t millis() {
+	return (systicks);
+}
+
+void printRegister(ModbusMaster& node, uint16_t reg) {
+	uint8_t result;
+	// slave: read 16-bit registers starting at reg to RX buffer
+	result = node.readHoldingRegisters(reg, 1);
+
+	// do something with data if read is successful
+	if (result == node.ku8MBSuccess)
+	{
+		printf("R%d=%04X\n", reg, node.getResponseBuffer(0));
+	}
+	else {
+		printf("R%d=???\n", reg);
+	}
+}
+
+bool setFrequency(ModbusMaster& node, uint16_t freq) {
+	uint8_t result;
+	int ctr;
+	bool atSetpoint;
+	const int delay = 500;
+
+	node.writeSingleRegister(1, freq); // set motor frequency
+
+	printf("Set freq = %d\n", freq/40); // for debugging
+
+	// wait until we reach set point or timeout occurs
+	ctr = 0;
+	atSetpoint = false;
+	do {
+		Sleep(delay);
+		// read status word
+		result = node.readHoldingRegisters(3, 1);
+		// check if we are at setpoint
+		if (result == node.ku8MBSuccess) {
+			if(node.getResponseBuffer(0) & 0x0100) atSetpoint = true;
+		}
+		ctr++;
+	} while(ctr < 20 && !atSetpoint);
+
+	printf("Elapsed: %d\n", ctr * delay); // for debugging
+
+	return (atSetpoint);
+}
+
+void Sleep(int ms)
+{
+	counter = ms;
+	while(counter > 0) {
+		__WFI();
+	}
+}
+
+int main(void) {
+
+	// Alustetaan nappi up
+	Chip_IOCON_PinMuxSet(LPC_IOCON, 0, 10, (IOCON_MODE_PULLUP | IOCON_DIGMODE_EN | IOCON_INV_EN));
+	Chip_GPIO_SetPinDIRInput(LPC_GPIO, 0, 10);
+
+	// Alustetaan nappi down
+	Chip_IOCON_PinMuxSet(LPC_IOCON, 0, 16, (IOCON_MODE_PULLUP | IOCON_DIGMODE_EN | IOCON_INV_EN));
+	Chip_GPIO_SetPinDIRInput(LPC_GPIO, 0, 16);
+
+	// Alustetaan nappi ok
+	Chip_IOCON_PinMuxSet(LPC_IOCON, 1, 3, (IOCON_MODE_PULLUP | IOCON_DIGMODE_EN | IOCON_INV_EN));
+	Chip_GPIO_SetPinDIRInput(LPC_GPIO, 1, 3);
+
+	// Alustetaan nappi back
+	Chip_IOCON_PinMuxSet(LPC_IOCON, 0, 0, (IOCON_MODE_PULLUP | IOCON_DIGMODE_EN | IOCON_INV_EN));
+	Chip_GPIO_SetPinDIRInput(LPC_GPIO, 0, 0);
+
+
+#if defined (__USE_LPCOPEN)
+	// Read clock settings and update SystemCoreClock variable
+	SystemCoreClockUpdate();
+#if !defined(NO_BOARD_LIB)
+	// Set up and initialize all required blocks and
+	// functions related to the board hardware
+	Board_Init();
+	// Set the LED to the state of "On"
+	Board_LED_Set(2, true);
+#endif
+#endif
+
+
+	Chip_RIT_Init(LPC_RITIMER);
+	NVIC_EnableIRQ(RITIMER_IRQn);
+
+	SysTick_Config(Chip_Clock_GetSysTickClockRate() / 1000);
+
+	Chip_SWM_MovablePortPinAssign(SWM_SWO_O, 1, 2);
+
+
+	ModbusMaster node(2); // Create modbus object that connects to slave id 2
+
+	node.begin(9600); // set transmission rate - other parameters are set inside the object and can't be changed here
+	printRegister(node, 3); // for debugging
+	node.writeSingleRegister(0, 0x0406); // prepare for starting
+	printRegister(node, 3); // for debugging
+
+	Sleep(1000); // give converter some time to set up
+	// note: we should have a startup state machine that check converter status and acts per current status
+	//       but we take the easy way out and just wait a while and hope that everything goes well
+
+	printRegister(node, 3); // for debugging
+	node.writeSingleRegister(0, 0x047F); // set drive to start mode
+	printRegister(node, 3); // for debugging
+
+	Sleep(1000); // give converter some time to set up
+	// note: we should have a startup state machine that check converter status and acts per current status
+	//       but we take the easy way out and just wait a while and hope that everything goes well
+
+	printRegister(node, 3); // for debugging
+
+
+	int i = 0;
+	int j = 0;
+	const uint16_t fa[20] = { 1000, 2000, 3000, 3500, 4000, 5000, 7000, 8000, 8300, 10000, 10000, 9000, 8000, 7000, 6000, 5000, 4000, 3000, 2000, 1000 };
+
+	LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
+
+	lcd.begin(16,2);
+	lcd.setCursor(0,0);
+
+	SimpleMenu menu;
+
+	// Alamenun otsikkonimikkeet. Rajat annetaan samassa järjestyksessä.
+	//vector <string> setupvalikot = { "Min", "Max", "Hertz", "Time set" };
+	//vector <string> statusvalikot = { "info_System", "info_ModBus", "info_Pressure_sensor"};
+
+	static const string arr[] = { "Min", "Max", "Hertz", "Time set" };
+	vector<string> setupvalikot (arr, arr + sizeof(arr) / sizeof(arr[0]) );
+
+	static const string arr2[] ={ "info_System", "info_ModBus", "info_Pressure_sensor"};
+	vector<string> statusvalikot (arr2, arr2 + sizeof(arr2) / sizeof(arr2[0]) );
+
+
+	// Alamenun kohtien ylarajat, muista sama järjestys!
+	//vector <int> ylarajat = { 10, 5, 2, 50 };
+
+	// Alamenun kohtien alarajat, muista sama järjestys!
+	//vector <int> alarajat = { 0, 0, 0, 0 };
+
+
+	static const int arr3[] = { 10, 5, 2, 50 };
+	vector<int> ylarajat (arr3, arr3 + sizeof(arr3) / sizeof(arr3[0]) );
+
+	static const int arr4[] = { 0, 0, 0, 0 };
+	vector<int> alarajat (arr4, arr4 + sizeof(arr4) / sizeof(arr4[0]) );
+
+
+
+	// Luodaan menun kohdat. Menuille annetaan otsikko, alamenujen otsikot, alarajat ja ylarajat.
+	OnOffEdit power = OnOffEdit(lcd, "Power");
+	ManuAutoEdit mode = ManuAutoEdit(lcd, "Mode");
+	SetupEdit setup = SetupEdit(lcd, "Setup", setupvalikot, alarajat, ylarajat);
+	StatusEdit status = StatusEdit(lcd, "Status", statusvalikot, alarajat, ylarajat);
+
+	// Päävalikkoon lisätään kohtia. True / False viimeisenä parametrina kertoo, onko valikolla alavalikko.
+	menu.addItem(new SubMenuItem(power, false));
+	menu.addItem(new SubMenuItem(mode, false));
+	menu.addItem(new SubMenuItem(setup, true));
+	menu.addItem(new SubMenuItem(status, true));
+
+	// Display first menu item
+	menu.event(SubMenuItem::show);
+
+	//I2C i2c = I2C(0, );
+
+	int lukema = 0;
+
+	while(1){
+
+			/*  TAAJUUDEN VAIHTELU KOMMENTOITU
+			uint8_t result;
+
+			// slave: read (2) 16-bit registers starting at register 102 to RX buffer
+			j = 0;
+			do {
+				result = node.readHoldingRegisters(102, 2);
+				j++;
+			} while(j < 3 && result != node.ku8MBSuccess);
+
+			if (result == node.ku8MBSuccess) {
+				printf("F=%4d, I=%4d  (ctr=%d)\n", node.getResponseBuffer(0), node.getResponseBuffer(1),j);
+			}
+			else {
+				printf("ctr=%d\n",j);
+			}
+
+
+			Sleep(3000);
+			i++;
+			if(i >= 20) {
+				i=0;
+			}
+
+			// frequency is scaled:
+			// 20000 = 50 Hz, 0 = 0 Hz, linear scale 400 units/Hz
+			setFrequency(node, fa[i]);
+			 */
+
+			// VALIKKO
+			if (Chip_GPIO_GetPinState(LPC_GPIO, 0, 10)) {
+				while (Chip_GPIO_GetPinState(LPC_GPIO, 0, 10)) {
+				}
+				menu.event(SubMenuItem::up);
+			}
+			if (Chip_GPIO_GetPinState(LPC_GPIO, 0, 16)) {
+				while (Chip_GPIO_GetPinState(LPC_GPIO, 0, 16)) {
+				}
+				menu.event(SubMenuItem::down);
+			}
+			if (Chip_GPIO_GetPinState(LPC_GPIO, 1, 3)) {
+				while (Chip_GPIO_GetPinState(LPC_GPIO, 1, 3)) {
+				}
+				menu.event(SubMenuItem::ok);
+			}
+			if (Chip_GPIO_GetPinState(LPC_GPIO, 0, 0)) {
+				while (Chip_GPIO_GetPinState(LPC_GPIO, 0, 0)) {
+				}
+				menu.event(SubMenuItem::back);
+			}
+
+			if (mode.getValue() == "Automatic"){
+				// Automaattiohjaus
+			}
+
+			if (mode.getValue() == "Manual"){
+				// Manuaaliohjaus
+			}
+
+
+			// Näin muutetaan statuksen arvoja:
+			// Ensimmäinen parametri määrittää monesko valikon kohta on kyseessä. Alkaen nollasta.
+			// 0 = info_System, 1 = info_ModBus, 2 = info_Pressure_sensor
+			//	if (jotain){
+			//		status.setValue[0, "Running OK"]
+			//	}
+
+			//	if (jotain){
+			//	status.setValue[1, "Connected"]
+			//	}
+
+			// Näin tarkastetaan asetettuja arvoja:
+			// Tällä voisi tarkistaa, onko ala-arvo muuttunut nykyisestä.
+			//	if(setup.getValue(0) != asetettu_ala_arvo){
+			// Mitä tapahtuu jos arvo on muuttunut
+			//	}
+	}
+	return (0);
+}
+>>>>>>> parent of 3a4741b... v0.91
